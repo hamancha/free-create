d06445a8fecd489f8674bddff1ebd024b61f6107
Harshit Manchanda <hamancha@microsoft.com>
2021-03-18 15:32:41 -0700

    playfab style pod info

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module free-create
 go 1.16
 
 require (
+	k8s.io/api v0.20.4
 	k8s.io/apimachinery v0.20.4
 	k8s.io/client-go v0.20.4
 )
--- podgetter/config-info.go
+++ podgetter/config-info.go
@@ -7,10 +7,26 @@ import (
 	"net/http"
 	"time"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
+
+type Application struct {
+	name           string
+	description    string
+	commit         string
+	pod_count      int
+	oldest_pod_age string
+	status         string
+}
+
+type Cluster struct {
+	name         string
+	description  string
+	applications map[string]Application
+}
 
 // GetPods returns pod information
 func GetPods(w http.ResponseWriter) string {
@@ -24,6 +40,7 @@ func GetPods(w http.ResponseWriter) string {
 	if err != nil {
 		log.Println(err.Error())
 	}
+
 	// gets pods using selectors
 	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{LabelSelector: "app=free-create-app"})
 	if err != nil {
@@ -45,4 +62,38 @@ func GetPods(w http.ResponseWriter) string {
 		}
 	}
 	return fmt.Sprintf("There are %d pods in the cluster", len(pods.Items))
+}
+
+func buildPodInformation(clusterName string, clusterDescription string, pods *v1.PodList) Cluster {
+	var clusterInfo Cluster
+	clusterInfo.name = clusterName
+	clusterInfo.description = clusterDescription
+	for _, pod := range pods.Items {
+		podStatusPhase := string(pod.Status.Phase)
+		podCreationTime := pod.GetCreationTimestamp()
+		podAge := time.Since(podCreationTime.Time).Round(time.Second)
+		labels := pod.GetLabels()
+		if application, ok := clusterInfo.applications[labels["playfab_application"]]; ok {
+			application.pod_count++
+			application.commit = labels["pf-main/commit"]
+			if podStatusPhase != application.status && podStatusPhase != "Running" {
+				application.status = podStatusPhase
+			}
+			currentOldestDuration, _ := time.ParseDuration(application.oldest_pod_age)
+			if podAge > currentOldestDuration {
+				application.oldest_pod_age = podAge.String()
+			}
+		} else {
+			unRegisteredApp := Application{
+				name:           labels["playfab_application"],
+				commit:         labels["pf-main/commit"],
+				pod_count:      1,
+				oldest_pod_age: podAge.String(),
+				status:         podStatusPhase,
+			}
+			clusterInfo.applications[labels["playfab_application"]] = unRegisteredApp
+		}
+	}
+
+	return clusterInfo
 }
